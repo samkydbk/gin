a35c0c7e329afd71247418540ae44421942ecb3b
zhang.yongtao <zhang.yongtao1@byd.com>
2021-02-05 11:07:52 +0800

    增删改查学习完成

--- controllers/user.go
+++ controllers/user.go
@@ -27,7 +27,7 @@ func UserList(c *gin.Context) {
 //创建用户
 func CreateUser(c *gin.Context) {
 	var model models.User
-	model.Name = "abc"
+	model.Name = "eeeee"
 	model.Sex = 1
 	model.Phone = "13228016321"
 	_, err := logics.CreateUser(model)
@@ -39,8 +39,19 @@ func CreateUser(c *gin.Context) {
 
 //查询用户
 func SearchUser(c *gin.Context) {
+	//获取post表单信息
+	//id := c.PostForm("id")
+	//fmt.Println("id:", id)
+
+	//获取get信息 请求方式一
+	//id, _ := strconv.ParseInt(c.Param("id"), 10, 64)
+	//fmt.Println("id:", c.Param("id"))
+
+	//获取get信息 请求方式二
+	//fmt.Println("id:", c.Query("id"))
+
 	//将字符串转换为数字函数ParseInt 字符串,进制,返回大小
-	id, _ := strconv.ParseInt(c.Param("id"), 10, 64)
+	id, _ := strconv.ParseInt(c.Query("id"), 10, 64)
 	res, err := logics.SearchUser(id)
 	if err != nil {
 		panic(err)
@@ -51,7 +62,7 @@ func SearchUser(c *gin.Context) {
 
 //修改用户
 func UpdateUser(c *gin.Context) {
-	id, _ := strconv.ParseInt(c.Param("id"), 10, 64)
+	id, _ := strconv.ParseInt(c.PostForm("id"), 10, 64)
 	var model models.User
 	model.Phone = "13288888888"
 	_, err := logics.UpdateUser(model, id)
--- routers/router.go
+++ routers/router.go
@@ -14,8 +14,16 @@ func InitRouter() *gin.Engine {
 	//用户增删改查及列表
 	router.GET("/user/list", controllers.UserList)
 	router.POST("/user/create", controllers.CreateUser)
-	router.POST("/user/search/:id", controllers.SearchUser)
-	router.POST("/user/update/:id", controllers.UpdateUser)
+	//post请求
+	//router.POST("/user/search", controllers.SearchUser)
+	//get请求方式一  127.0.0.1:8081/user/search/5  seo友好请求方式
+	//router.GET("/user/search/:id", controllers.SearchUser)
+	//get请求方式二  127.0.0.1:8081/user/search?id=8
+	//router.GET("/user/search", controllers.SearchUser)
+
+	router.GET("/user/search", controllers.SearchUser)
+
+	router.POST("/user/update", controllers.UpdateUser)
 	router.GET("/user/del/:id", controllers.DelUser)
 	return router
 }
